fd98a8d26b75304b7e2ef68ac5a8adceb3775688
Tarun Agarwal <tarunagarwal1981@gmail.com>
2025-09-16 10:31:15 +0530

    1

--- src/app/operator/packages/view/page.tsx
+++ src/app/operator/packages/view/page.tsx
@@ -438,8 +438,8 @@ export default function PackageDetailPage() {
                             <div>
                               <p className="text-sm text-gray-500">Flight Class</p>
                               <p className="font-semibold">{packageData.flightClass}</p>
-                            </div>
-                          </div>
+                        </div>
+                      </div>
                         )}
                       </>
                     )}
@@ -447,6 +447,8 @@ export default function PackageDetailPage() {
                 </CardContent>
               </Card>
             )}
+
+          </div>
 
           {/* Sidebar */}
           <div className="space-y-6">
